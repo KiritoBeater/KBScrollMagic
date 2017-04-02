748ec8a624154ce918a7de55a213b98db500c8a8
李阳 <1353137283@qq.com>
2017-04-03 00:47:35 +0800

    修改Demo

---
+++ Example/KBScrollMagic/SubGridCell.swift
@@ -0,0 +1,24 @@
+//
+//  SubGridCell.swift
+//  KBScrollMagic
+//
+//  Created by liyang on 2017/4/2.
+//  Copyright © 2017年 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class SubGridCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
